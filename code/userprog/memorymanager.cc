5b0513287588a389f383800e892695599366a106
JiaMingLin <robin890650@gmail.com>
2018-01-13 22:07:49 +0800

    mv memory manager

---
+++ code/userprog/memorymanager.cc
@@ -0,0 +1,4 @@
+#include "copyright.h"
+#include "main.h"
+
+Practice
---
+++ code/userprog/memorymanager.h
@@ -0,0 +1,42 @@
+// practice.h 
+//	Data structures to keep track of executing user programs 
+//	(address spaces).
+//
+//	For now, we don't keep any information about address spaces.
+//	The user level CPU state is saved and restored in the thread
+//	executing the user program (see thread.h).
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+// addrspace.h 
+//	Data structures to keep track of executing user programs 
+//	(address spaces).
+//
+//	For now, we don't keep any information about address spaces.
+//	The user level CPU state is saved and restored in the thread
+//	executing the user program (see thread.h).
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#ifndef PRACTICE_H
+#define PRACTICE_H
+
+#include "copyright.h"
+
+
+class Practice {
+  public:
+    Practice();	
+    ~Practice();
+
+    int Translate(int virtAddr);
+
+  private:
+
+};
+
+#endif 
